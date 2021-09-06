9c046f507b307d64904df5a8ffb2414f1d985ff1
AllisonAnz <AllisonAnzalone@gmail.com>
2021-09-06 11:36:48 -0500

    commit tree 7d8168de4218eb3cc929bb54ce888f2697a618a0

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,13 @@ require 'active_record'
 
 require_relative '../lib/dog'
 
+# Tells ActiveRecord where the database is located 
+# Once eastablish_connection is ran, ActiveRecord::Base keeps it stored as a class 
+# variable at ActiveRecord::Base.connection
 ActiveRecord::Base.establish_connection(
+  # adapter tells ActiveRecord which SQL software to use
   adapter: "sqlite3",
+  # database tells ActiveRecord where the db is
   database: "./db/test.sqlite3"
 )
+
